ac8aaaacd7f598ab34418833fc7fda362935c5e8
York Shaw <york.p.shaw@gmail.com>
2023-02-06 14:05:44 -0600

    Shuffle the Array

---
+++ Shuffle The Array.js
@@ -0,0 +1,30 @@
+// #1470
+var shuffle = function(nums, n) {
+
+    if (n === 0) {
+        return [];
+    }
+
+    if (n === 1) {
+        return nums;
+    }
+
+    let shuffledArray = [];
+    let j = (nums.length / 2);
+
+    for (let i = 0; i < nums.length / 2; i++) {
+        shuffledArray.push(nums[i], nums[j]);
+        j++;
+    }
+
+    return shuffledArray;
+
+};
+
+// All lists are even
+// Edge: If n is 0, return an empty list
+// Edge: If n is 1, the list is 2 elements long...[x1, y1] return nums
+// Split the array in half
+// Rejoin the arrays, one element at a time from each
+// j is always nums.length / 2
+// O(n) beause you are checking each value once?
