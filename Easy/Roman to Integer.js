6664829ca26494756b4941a2737a9bdf2c31c2a8
York Shaw <york.p.shaw@gmail.com>
2023-02-08 13:55:47 -0600

    Roman to Integer solution 2/8/23

--- Easy/Roman to Integer.js
+++ Easy/Roman to Integer.js
@@ -4,19 +4,40 @@
  */
 var romanToInt = function(s) {
 
-    const roman_nums = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000}
-    const num_adjust = {IV: 2, IX: 2, XL: 20, XC: 20, CD: 200, CM: 200 }
-    let count = 0
+    const numerals = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+  };
 
-    console.log(count)
+  let integer = 0;
+
+  for (let i = 0; i < s.length; i++) {
+      let cur = numerals[s[i]];
+      let next = numerals[s[i+1]]
+      if (cur < next) {
+          integer -= cur;
+      } else {
+          integer += cur;
+      }
+  }
+  return integer;
 };
 
 
-
 //Number length at least 1, no more than 15
-//Create a count variable
-//IV(6) -> 4, IX(11) -> 9, XL(60) -> 40, XC(110) -> 90,
-//CD(600) -> 400, CM(1100) -> 900
-//Each time IV or IX appears, subtract 2 from count
-//Each time XL or XC appears, subtract 20 from count
-//Each time CD or CM appears, subtract 200 from count
+//Create a counting variable
+//IV(4) is V-I, IX(9) is X-I, XL(40) is L-X, XC(90) ix C-X,
+//CD(400) is D-C, CM(900) is M-C
+//Roman Integers are sorted from largest to smallest
+//Iterate through string S
+//Set two pointers, one for the curr and one for the next
+//If the curr element is smaller than the next (ie, IV or IX), subtract current from count
+
+//MCMXCIV - M CM XC IV - 1000+900+90+4 = 1994
+//1000(M) -100(C) +1000(M) -10(X) +100(C) -1(I) +5(V)
+//1000->900->1900->1890->1990->1989->1994
