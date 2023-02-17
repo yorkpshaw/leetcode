3503ba576ab705689e4ebc7e680cbcdcaad15b72
York Shaw <york.p.shaw@gmail.com>
2023-02-17 09:26:16 -0600

    2/17/23 Sum of Unique Elements

---
+++ Easy/Sum of Unique Elements.js
@@ -0,0 +1,57 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfUnique = function(nums) {
+    // Hash Table
+     const freq = {};
+      let res = 0;
+
+      for (let i = 0; i < nums.length; i++) {
+        if (freq[nums[i]] === undefined) {
+          freq[nums[i]] = 1;
+        } else {
+          freq[nums[i]]++;
+        }
+      }
+
+      for (let i = 0; i < nums.length; i++) {
+        if (freq[nums[i]] === 1) {
+          res += nums[i];
+        }
+      }
+
+      return res;
+
+    };
+
+
+    // Brute force:
+    // let res = 0;
+    //     for(let i = 0; i < nums.length; i++){
+    //         if(nums.indexOf(nums[i]) !== nums.lastIndexOf(nums[i])){
+    //             continue;
+    //         };
+    //         res += nums[i];
+    //     };
+    //     return res;
+
+    // Map:
+    //  const freq = new Map();
+    //   let res = 0;
+
+    //   for (let i = 0; i < nums.length; i++) {
+    //     if (!freq.has(nums[i])) {
+    //       freq.set(nums[i], 1);
+    //     } else {
+    //       freq.set(nums[i], freq.get(nums[i]) + 1);
+    //     }
+    //   }
+
+    //   for (let i = 0; i < nums.length; i++) {
+    //     if (freq.get(nums[i]) === 1) {
+    //       res += nums[i];
+    //     }
+    //   }
+
+    //   return res;
