9b1a570a887b253cad6219fa102b39ac6205ddef
York Shaw <york.p.shaw@gmail.com>
2023-02-18 16:54:46 -0600

    Missing number

---
+++ Easy/Map/Missing Number.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+
+    let map = new Set(nums);
+
+    let expectedLength = nums.length + 1
+
+    for (let number = 0; number < expectedLength; number++) {
+        if (!map.has(number)) {
+            return number
+        }
+    }
+
+    return -1
+
+
+
+};
