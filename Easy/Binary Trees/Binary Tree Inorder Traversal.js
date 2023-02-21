e4b1cd5f9dd2c5a9172a1b4771cf77a0f0667397
York Shaw <york.p.shaw@gmail.com>
2023-02-21 13:47:41 -0600

    2/21/23 BT Inorder Traversal

---
+++ Easy/Binary Trees/Binary Tree Inorder Traversal.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+
+    let result = [];
+    inorder(root);
+    function inorder(root) {
+        if (!root) {
+            return null;
+        }
+        inorder(root.left);
+        result.push(root.val);
+        inorder(root.right);
+    }
+    return result;
+};
