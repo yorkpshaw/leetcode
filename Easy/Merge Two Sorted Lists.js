bb2044eb3ead9e25bdcb8a3e6039a5492fecb213
York Shaw <york.p.shaw@gmail.com>
2023-02-22 23:14:01 -0600

    2/22/23 merge two sorted lists

---
+++ Easy/Merge Two Sorted Lists.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+
+    if (list1 == null) return list2;
+    if (list2 == null) return list1;
+
+    // If l1 is lower
+    if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    }
+    // If l2 is lower
+    else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+    }
+
+};
