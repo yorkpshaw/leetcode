bd5e8e1230df5e977e9116d2a324a59e9936c3e0
York Shaw <york.p.shaw@gmail.com>
2023-02-25 10:13:43 -0600

    2/25/23 fixed climbing stairs

--- Easy/Climbing Stairs.js
+++ Easy/Climbing Stairs.js
@@ -3,14 +3,35 @@
  * @return {number}
  */
 var climbStairs = function(n) {
+    if (n < 2) {
 
-    //base case
-if (n <= 1) return 1;
+        return 1;
+    }
 
-//otherwise this is basically fib sequence f(n) = f(n-1) + f(n-2)
-return climbStairs(n - 1) + climbStairs(n - 2);
+        let firstStep = 1;
 
+        let secondStep = 1;
 
-};
+        let thirdStep = 0;
 
-// Fibonacci Sequence
+        for (let i=2; i<=n; i++) {
+
+            thirdStep = firstStep + secondStep;
+
+            firstStep = secondStep;
+
+            secondStep = thirdStep;
+        }
+
+        return thirdStep;
+
+    };
+
+    // Recursion solution:
+    // var climbStairs = function(n) {
+
+    //     if (n <= 1)
+    //         return 1;
+    //     return climbStairs(n-1) + climbStairs(n-2);
+
+    // };
