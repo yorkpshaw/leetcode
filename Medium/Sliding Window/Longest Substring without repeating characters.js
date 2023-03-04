4175f79d1fea66f1f422a70f80eccf7049c5ddf8
York Shaw <york.p.shaw@gmail.com>
2023-03-03 18:05:51 -0600

    longest substring without repeating characters

---
+++ Medium/Sliding Window/Longest Substring without repeating characters.js
@@ -0,0 +1,37 @@
+function lengthOfLongestSubstring(str) {
+
+    let longest = 0;
+    let seen = {};
+    let start = 0;
+
+    for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+        start = Math.max(start, seen[char]);
+    }
+    // index - beginning of substring + 1 (to include current in count)
+    longest = Math.max(longest, i - start + 1);
+    // store the index of the next char so as to not double count
+    seen[char] = i + 1;
+    }
+  return longest;
+}
+
+
+//   let longest = 0;
+//   let substrings = new Set();
+//   let start = 0;
+//   let end = 0;
+
+//   while (start < s.length && end < s.length) {
+//     if (!substrings.has(s[end])) {
+//       substrings.add(s[end]);
+//       end++;
+//       longest = Math.max(longest, end - start);
+//     } else {
+//       substrings.delete(s[start]);
+//       start++;
+//     }
+//   }
+
+//   return longest;
