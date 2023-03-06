c3288e8585fb27572eb11ea648fe221e1f97be05
York Shaw <york.p.shaw@gmail.com>
2023-03-06 11:21:30 -0600

    Updated valid palindrome

--- Easy/Valid Palindrome.js
+++ Easy/Valid Palindrome.js
@@ -4,36 +4,61 @@
  */
 var isPalindrome = function(s) {
 
-    // Base case
-        if(s==null)  return false;
-        // Take in lowercase character...
-        s = s.toLowerCase();
-        // Initialize two pointer variables, left and right and point them with the two ends of the input string...
+        s = s.replace(/[^a-z0-9]/gi,"") // Get rid of all special characters and cases
         let left = 0;
-        let right = s.length - 1;
-        // Traverse all elements through the loop...
-        while(left < right){
-            // Move the left pointer to right so it points to a alphanumeric character...
-            while(left < right && !((s.charAt(left) >= 'a' && s.charAt(left) <= 'z') || (s.charAt(left) >= '0' && s.charAt(left) <= '9'))){
-                left++;
-            }
-            // Similarly move right pointer to left so it also points to a alphanumeric character...
-            while(left < right && !((s.charAt(right) >= 'a' && s.charAt(right) <= 'z') || (s.charAt(right) >= '0' && s.charAt(right) <= '9'))){
-                right--;
-            }
-            // Check if both the characters are same...
-            // If it is not equal then the string is not a valid palindrome, hence return false...
-            if(s.charAt(left) != s.charAt(right)){
+        let right = s.length - 1
+        while (left < right) {
+            if (s[left].toLowerCase() !== s[right].toLowerCase()) { // Lowercase these to get rid of conflict
                 return false;
-            }
-            // If same, then continue to next iteration and move both pointers to point to next alphanumeric character till left < right...
             left++;
             right--;
+            }
         }
-        // After loop finishes, the string is said to be palindrome, hence return true...
-        return true;
+        return true
 
-    };
+    }
+    // };
+
+    // Time:
+    // Space:
+    // Pseudo Code:
+    // 1. Create regex to replace special characters with ""
+    // 2. Create a left/start pointer
+    // 3. Create a right/end pointer
+    // 4. Iterate until left < right
+    //     -Create condition to see if values of pointers do not equal each other, return false if so
+    //     -Increment left pointer, decrement right pointer
+    // 5. return true if nothing has been proven false
+
+    // // Base case
+    // if(s==null)  return false;
+    // // Take in lowercase character...
+    // s = s.toLowerCase();
+    // // Initialize two pointer variables, left and right and point them with the two ends of the input string...
+    // let left = 0;
+    // let right = s.length - 1;
+    // // Traverse all elements through the loop...
+    // while(left < right){
+    //     // Move the left pointer to right so it points to a alphanumeric character...
+    //     while(left < right && !((s.charAt(left) >= 'a' && s.charAt(left) <= 'z') || (s.charAt(left) >= '0' && s.charAt(left) <= '9'))){
+    //         left++;
+    //     }
+    //     // Similarly move right pointer to left so it also points to a alphanumeric character...
+    //     while(left < right && !((s.charAt(right) >= 'a' && s.charAt(right) <= 'z') || (s.charAt(right) >= '0' && s.charAt(right) <= '9'))){
+    //         right--;
+    //     }
+    //     // Check if both the characters are same...
+    //     // If it is not equal then the string is not a valid palindrome, hence return false...
+    //     if(s.charAt(left) != s.charAt(right)){
+    //         return false;
+    //     }
+    //     // If same, then continue to next iteration and move both pointers to point to next alphanumeric character till left < right...
+    //     left++;
+    //     right--;
+    // }
+    // // After loop finishes, the string is said to be palindrome, hence return true...
+    // return true;
+
 
 
 
