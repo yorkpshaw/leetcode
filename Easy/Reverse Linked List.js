e5a3ccc5414fe139d07fd2882427165c34ef9fcf
York Shaw <york.p.shaw@gmail.com>
2023-03-07 06:15:20 -0600

    3/7/23

--- Easy/Reverse Linked List.js
+++ Easy/Reverse Linked List.js
@@ -22,3 +22,42 @@ var reverseList = function(head) {
     }
         return prev;
     };
+
+    // reverse(){
+    //     var node = this.head;
+    //     this.head = this.tail;
+    //     this.tail = node;
+    //     var next;
+    //     var prev = null;
+    //     for(var i = 0; i < this.length; i++){
+    //       next = node.next;
+    //       node.next = prev;
+    //       prev = node;
+    //       node = next;
+    //     }
+    //     return this;
+
+    // 1 -> 2 -> 3 -> 4 -> null
+    // Head node is the argument being passed
+    // Set prev/next (or left/right) pointers to be null
+    // Set the head to the current pointer
+    // Loop while current !== null which would mean you're at the end
+
+    // Set next to current.next next = 2 -> 3 -> 4 -> null
+    // Set current.next to previous current = 1 -> null
+    // Set previous to current prev = 1 -> null
+    // Set current to next current = 2 -> 3 -> 4 -> null
+    // Repeat until the end is reached
+
+    // Return prev as the new head of the reversed list
+    // prev = 2 -> 1 -> null
+    // next = 3 -> 4 -> null
+    // current = 3 -> 4 -> null
+
+    // prev = 3 -> 2 -> 1 -> null
+    // next = 4 -> null
+    // current = 4 -> null
+
+    // prev = 4 -> 3 -> 2 -> 1 -> null
+    // next = null
+    // current = null
