2e0c0f379b296b4de9db71c8c4441cb33e0921e7
York Shaw <york.p.shaw@gmail.com>
2023-03-12 13:58:29 -0500

    3/12/23 power of three

---
+++ Easy/Power of Three.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+
+    while(n > 1){
+           n /= 3
+       }
+
+       return n === 1
+
+   };
+
+//    The division assignment (/=) operator divides a variable by the value of the right operand and assigns the result to the variable.
