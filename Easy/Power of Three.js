6eca0dbfc8c7f8a9cf51ffac053216f69f0834ec
York Shaw <york.p.shaw@gmail.com>
2023-03-21 08:54:48 -0500

    3/21/23 reverse integer

--- Easy/Power of Three.js
+++ Easy/Power of Three.js
@@ -12,4 +12,7 @@ var isPowerOfThree = function(n) {
 
    };
 
+
+console.log(isPowerOfThree(10))
+
 //    The division assignment (/=) operator divides a variable by the value of the right operand and assigns the result to the variable.
