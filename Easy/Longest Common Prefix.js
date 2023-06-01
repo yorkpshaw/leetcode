261748b6a3f331f284920c6c9e7dd95041db10fd
York Shaw <york.p.shaw@gmail.com>
2023-06-01 11:06:29 -0500

    max subarray

--- Easy/Longest Common Prefix.js
+++ Easy/Longest Common Prefix.js
@@ -2,42 +2,30 @@
  * @param {string[]} strs
  * @return {string}
  */
-// var longestCommonPrefix = function(strs) {
-
-//     const firstString = strs[0].split('');
-//     let i = 0;
-//     let mcp = '';
-
-//     while (firstString[i]) {
-//         for (let j = 1; j < strs.length; j++) {
-//             let secondString = strs[j].split('');
-//             if (secondString[j] == firstString[i]) {
-//                 mcp += secondString[j];
-//             }
-//             i++
-//         }
-//         return mcp;
-//     }
-// };
 var longestCommonPrefix = function(strs) {
-    if (strs.length === 0) return '';
 
-    let mcp = strs[0];
+    let lcp = strs[0];
 
     for (let i = 1; i < strs.length; i++) {
-        while (strs[i].indexOf(mcp) !== 0) {
-            mcp = mcp.substring(0, mcp.length - 1);
+        while (strs[i].indexOf(lcp) !== 0) {
+            lcp = lcp.substring(0, lcp.length-1);
         }
     }
 
-    return mcp;
+    return lcp;
 };
 
-// Create an empty string to be pushed into
-// Split the first element's characters
-// Iterate over all the first element's characters
-// Loop through the rest doing the same
-// While iterating,
-// When characters do not match, stop the loop
-// When everything is finished looping, return the output string
-// Return an output string with longest common prefix
+/*
+
+["flower", "flow", "flight"] (flower becomes fl on the first iteration)
+["flower", "flowerful", "flowerfully"] (indexOf will always be 0)
+Compare the first string of each array with the other strings
+Create variable to store that first string
+
+Using indexOf, which returns the index that a string first starts at, compare first string to others
+While indexOf first string is NOT equal to 0 (meaning all of first string doesn't fully fit)
+use substring to slice off the last character
+keep chopping off the last character until that first string can fit into whatever is next
+make sure to reassign variable
+
+ */
