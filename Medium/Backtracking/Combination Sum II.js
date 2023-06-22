0195025252578e1549d389d5ed0f64bce318b401
York Shaw <york.p.shaw@gmail.com>
2023-06-22 07:44:20 -0500

    Combo Sum II

--- Medium/Backtracking/Combination Sum II.js
+++ Medium/Backtracking/Combination Sum II.js
@@ -1,1 +1,32 @@
-/* https://leetcode.com/problems/combination-sum-ii/ */
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(candidates, target) {
+    
+    const result = [];
+
+    candidates.sort((a, b) => a - b);
+
+    const dfs = (i, candidates, target, slate) => {
+
+        if (target < 0) return;
+
+        if (target === 0) {
+            result.push(slate.slice());
+            return;
+        }
+
+        for (let j = i; j < candidates.length; j++) {
+            if (i !== j && candidates[j] === candidates[j - 1]) continue; // Check if there is a duplicate. Different indices but same value means you don't need to visit that tree path
+            slate.push(candidates[j]);
+            dfs(j + 1, candidates, target - candidates[j], slate);
+            slate.pop();
+        }
+
+    };
+
+    dfs(0, candidates, target, []);
+    return result;
+};
